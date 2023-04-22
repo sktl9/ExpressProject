6aa1cc088c0d950c1035c8797e9250deac79b45b
sktl9 <sktl.gg@mail.ru>
2023-04-22 16:48:45 +0300

    verison2

--- PostController.js
+++ PostController.js
@@ -1,32 +1,72 @@
 import Post from "./Post.js"
+import PostService from "./PostService.js";
 
-class PostController{
-    async create(req, res){
-        try{  
-    const{author,title,content,picture} = req.body
-    const post = await Post.create({author,title,content,picture})
-    console.log(req.body);
-    res.json(post);
-    req.json('hi')
-    }catch(e){
-    res.status(500).json(e)
-    }}
-    async getAll(req,res){
-try{
-    const posts = await Post.find();
-    return res.json(posts);
-}catch(e){
-    res.status(500).json(e)
-}}
-    async getOne(req,res){
-        
+class PostController {
+    async create(req, res) {
+        try {
+            const post = await PostService.create(req.body)
+            res.json(post);
+        } catch (e) {
+            res.status(500).json(e)
+        }
     }
-    async update(req,res){
-        
+
+    async getAll(req, res) {
+        try {
+            const posts = await Post.find();
+            res.json(posts);
+        } catch (e) {
+            res.status(500).json(e)
+        }
     }
-    async delete(req,res){
-        
+    async getOne(req, res) {
+        try {
+            const post = await PostService.getOne(req.params.id)
+        } catch (e) {
+            res.status(500).json(e)
+        }
     }
+    async update(req, res) {
+        try {
+            const post = req.body
+            if (!post._id) { return res.status(400).json({ messege: 'id не передан' }) }
+            const updatedPost = await Post.findByIdAndUpdate(post._id, post, { new: true })
+            return res.json(updatedPost)
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    }
+    async delete(req, res) {
+        try {
+            const { id } = req.params
+            if (!id) { return res.status(400).json({ messege: 'id не передан' }) }
+            const post = await Post.findByIdAndDelete(id)
+            return res.json(post)
+        } catch (e) {
+
+            return res.status(500).json(e)
+        }
+    }
+    async updateOne(req, res) {
+        try {
+            const post = req.body
+            if (!post._id) { return res.status(400).json({ messege: 'id не указан' }) }
+            const UpdPost = await Post.findByIdAndUpdate(post._id, {
+                "author": "jedi",
+                "title": "hi",
+                "content": "hi world!",
+                "__v": 0
+            }, { new: true })
+            const author = post.author
+            if (!post.author){ return res.status(400).json({ messege: 'author не указан' }) }
+            const FindSerg = await Post.findOneAndDelete(post.author)
+            const FindAll = await Post.find()
+            return res.status(200).json(FindAll)
+        } catch (e) {
+            return res.status(500).json(e)
+        }
+    }
+
 
 }
 
--- router.js
+++ router.js
@@ -5,9 +5,8 @@ import PostController from './PostController.js'
 const router = new Router()
 
 router.post('/posts',PostController.create)
-  
 router.get('/posts',PostController.getAll)
-router.get('/posts',PostController.getAll)
+router.put('/update',PostController.updateOne)
 router.get('/posts/:id',PostController.getOne)
 router.put('/posts',PostController.update)
 router.delete('/posts/:id',PostController.delete)
